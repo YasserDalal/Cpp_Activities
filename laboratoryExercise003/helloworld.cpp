1cf7cb303fc4198dbc05f662eac3a438945cf855
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-04 18:25:41 +0300

    add questions to each functions

--- laboratoryExercise003/helloworld.cpp
+++ laboratoryExercise003/helloworld.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 
 bool yasserDalalErrors = NULL;
-
+/*
+  Write a program that displays “Hello” in one line, 
+  and “World” in another line using exactly two (2) COUT statements.
+*/
 int question1(){
   cout << "Hello" << endl;
   cout << "World" << endl;
   return yasserDalalErrors;
 }
 
+/*
+  Write a program that displays “Hello” in one line, 
+  and “World” in another line using exactly ten (10) COUT statements.
+*/
 int question2(){
   cout << "Hello" << endl;
   cout << "World" << endl;
@@ -24,6 +31,11 @@ int question2(){
   return yasserDalalErrors;
 }
 
+/*
+  Write a program that displays “Hello” in one line, 
+  and “World” in another line using exactly one (1) for the “Hello” 
+  and exactly five (5) COUT statements for the “World”.
+*/
 int question3(){
   std::cout << "Hello" << std::endl;
   std::cout << "World" << std::endl;
