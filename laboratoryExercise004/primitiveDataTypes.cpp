704adb6a0c70b442dc0b7d28ab571f4aa10bb24b
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-06 20:53:16 +0300

    answer question 1

--- laboratoryExercise004/primitiveDataTypes.cpp
+++ laboratoryExercise004/primitiveDataTypes.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+
+using namespace std;
+
+int question1() {
+  string userSection;
+
+  cout << "____________________________________________________________________________________" << endl;
+  cout << "" << endl;
+  
+  cout << "Question 1" << endl;
+  cout << "" << endl;
+
+  cout << "What is your Section?: "; cin >> userSection;
+  cout << "" << endl;
+
+  cout << "Section: " << userSection;
+  
+  cout << "" << endl;
+  cout << "____________________________________________________________________________________" << endl;
+
+  return false;
+}
