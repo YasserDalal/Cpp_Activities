9068fbfa5d59ed78f42b111b513cde01fa8a7248
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-09 00:06:28 +0300

    answer question 1

--- laboratoryExercise005/conditionalStatements.cpp
+++ laboratoryExercise005/conditionalStatements.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+
+using namespace std;
+
+int question1(){
+  int numInput;
+  string userInput;
+
+  cout << "Press a number from (0 - 4): "; getline(cin >> ws, userInput); 
+
+  numInput = stoi(userInput);
+
+  if(numInput == 0) { cout << "Hello World" << endl; return 0; }
+  if(numInput == 1) { cout << "I am Groot" << endl; return 0; }
+  if(numInput == 2) { cout << "To the Top" << endl; return 0; }
+  if(numInput == 3) { cout << "Where is the horizon" << endl; return 0; }
+  if(numInput == 4) { cout << "I do not know" << endl; return 0; }
+
+  cout << "Yeah, I will." << endl;
+  
+  return 0;
+}
