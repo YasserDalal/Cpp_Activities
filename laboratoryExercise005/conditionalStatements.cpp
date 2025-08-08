913facd755fda41229237cf67f8a1d01eb5f6695
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-09 00:21:23 +0300

    answer question 2

--- laboratoryExercise005/conditionalStatements.cpp
+++ laboratoryExercise005/conditionalStatements.cpp
@@ -20,3 +20,23 @@ int question1(){
   
   return 0;
 }
+
+int question2(){
+  int numInput;
+  string userInput;
+
+  cout << "Press a number from (0 - 4): "; getline(cin >> ws, userInput);
+
+  numInput = stoi(userInput);
+
+  switch(numInput) {
+    case 0: cout << "Hello World" << endl; break;
+    case 1: cout << "I am good" << endl; break;
+    case 2: cout << "To the Top" << endl; break;
+    case 3: cout << "Where is the horizon" << endl; break;
+    case 4: cout << "I do not know" << endl; break;
+    default: cout << "Yeah, I will." << endl;
+  }
+
+  return 0;
+}
