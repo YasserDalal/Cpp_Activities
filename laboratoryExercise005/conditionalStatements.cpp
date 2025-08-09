af1996edfe34191ab137ad3adc87437d62c9cac2
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-09 14:24:12 +0300

    add line breaks

--- laboratoryExercise005/conditionalStatements.cpp
+++ laboratoryExercise005/conditionalStatements.cpp
@@ -2,21 +2,29 @@
 
 using namespace std;
 
+const string lineBreak = "____________________________________________________________________________________";
+
 int question1(){
   int numInput;
   string userInput;
 
+  cout << lineBreak << endl;
+  cout << endl;
+  cout << "Question 1" << endl;
+  cout << endl;
+
   cout << "Press a number from (0 - 4): "; getline(cin >> ws, userInput); 
+  cout << endl; 
 
   numInput = stoi(userInput);
 
-  if(numInput == 0) { cout << "Hello World" << endl; return 0; }
-  if(numInput == 1) { cout << "I am Groot" << endl; return 0; }
-  if(numInput == 2) { cout << "To the Top" << endl; return 0; }
-  if(numInput == 3) { cout << "Where is the horizon" << endl; return 0; }
-  if(numInput == 4) { cout << "I do not know" << endl; return 0; }
+  if(numInput == 0) { cout << "Hello World" << endl << lineBreak << endl; return 0; }
+  if(numInput == 1) { cout << "I am Groot" << endl << lineBreak << endl; return 0; }
+  if(numInput == 2) { cout << "To the Top" << endl << lineBreak << endl; return 0; }
+  if(numInput == 3) { cout << "Where is the horizon" << endl << lineBreak << endl; return 0; }
+  if(numInput == 4) { cout << "I do not know" << endl << lineBreak << endl; return 0; }
 
-  cout << "Yeah, I will." << endl;
+  cout << "Yeah, I will." << endl << lineBreak << endl;
   
   return 0;
 }
@@ -25,17 +33,23 @@ int question2(){
   int numInput;
   string userInput;
 
+  cout << lineBreak << endl;
+  cout << endl;
+  cout << "Question 2" << endl;
+  cout << endl;
+
   cout << "Press a number from (0 - 4): "; getline(cin >> ws, userInput);
+  cout << endl;
 
   numInput = stoi(userInput);
 
   switch(numInput) {
-    case 0: cout << "Hello World" << endl; break;
-    case 1: cout << "I am good" << endl; break;
-    case 2: cout << "To the Top" << endl; break;
-    case 3: cout << "Where is the horizon" << endl; break;
-    case 4: cout << "I do not know" << endl; break;
-    default: cout << "Yeah, I will." << endl;
+    case 0: cout << "Hello World" << endl << lineBreak << endl; break;
+    case 1: cout << "I am good" << endl << lineBreak << endl; break;
+    case 2: cout << "To the Top" << endl << lineBreak << endl; break;
+    case 3: cout << "Where is the horizon" << endl << lineBreak << endl; break;
+    case 4: cout << "I do not know" << endl << lineBreak << endl; break;
+    default: cout << "Yeah, I will." << endl << lineBreak << endl;
   }
 
   return 0;
@@ -45,13 +59,22 @@ int question3(){
   float num1, num2;
   string input1, input2;
 
+  cout << lineBreak << endl;
+  cout << endl;
+  cout << "Question 3" << endl;
+  cout << endl;
+
   cout << "Enter your first number: "; getline(cin >> ws, input1);
+  cout << endl;
   cout << "Enter your second number: "; getline(cin >> ws, input2);
+  cout << endl;
 
   num1 = stof(input1);
   num2 = stof(input2);
 
   cout << "Result of " << num1 << " ÷ " << num2 << " = " << num1 / num2 << endl;
+
+  cout << lineBreak << endl;
 
   return 0;
 }
