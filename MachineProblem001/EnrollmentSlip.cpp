f628d3172d4bf7155810674b9c8c1413a2d3818e
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-11 17:33:01 +0300

    create an Enrollment Slip

--- MachineProblem001/EnrollmentSlip.cpp
+++ MachineProblem001/EnrollmentSlip.cpp
@@ -1,1 +1,66 @@
 #include <iostream>
+
+using namespace std;
+
+const string lineBreak = "_________________________________________________________________";
+
+int enrollSlip(){
+  const float thirtyPercent = 0.30;
+
+  long tuitionFee, balance, downPayment;
+  int yearLevel, ratePerUnit;
+  float units;
+  string studentName, course, yearString, unitString, yearName;
+
+  cout << "Student Name	        "   << ": "; getline(cin >> ws, studentName);
+  cout << "Program/Course	        " << ": "; getline(cin >> ws, course);
+  cout << "Year Level	        "     << ": "; getline(cin >> ws, yearString);
+  cout << "No. of Units	        "   << ": "; getline(cin >> ws, unitString);
+  cout << endl;
+  
+  yearLevel = stoi(yearString);
+  units = stof(unitString);
+
+  if(yearLevel == 1) {
+    yearName = "Freshman";
+    ratePerUnit = 1500;
+  } else if(yearLevel == 2) {
+    yearName = "Sophomore";
+    ratePerUnit = 1800;
+  } else if(yearLevel == 3) {
+    yearName = "Junior";
+    ratePerUnit = 2000;
+  } else if(yearLevel == 4 || yearLevel == 5) {
+    yearName = "Senior";
+    ratePerUnit = 2300;
+  } else {
+    cout << "Invalid year level: " << yearLevel << endl;
+    cout << endl;
+    cout << "Please enter a valid year level (1 - 5)";
+  }
+
+  tuitionFee = units * ratePerUnit;
+  downPayment = thirtyPercent * tuitionFee;
+  balance = tuitionFee - downPayment;
+
+  cout << lineBreak << endl;
+  cout << endl;
+  cout << "                    ENROLLMENT SLIP" << endl;
+  cout << endl;
+
+  cout << "Student Name	           " << ":" << studentName << endl;
+  cout << "Program/Course	           " << ":" << course << endl;
+  cout << "Year Name	           " << ":" << yearName << endl;
+  cout << "No. of Units	           " << ":" << units << endl;
+  cout << "Tuition Fee	           " << ":" << tuitionFee << endl;
+  cout << "Down Payment	           " << ":" << downPayment << endl;
+  cout << "Balance	                   " << ":" << balance << endl;
+
+  return 0;
+}
+
+int main(){
+  enrollSlip();
+
+  return 0;
+}
