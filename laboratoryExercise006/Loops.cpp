3196ddc142b264ef1b3fbf06a41df3ad297dc9fa
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-13 18:28:40 +0300

    answer question 1

--- laboratoryExercise006/Loops.cpp
+++ laboratoryExercise006/Loops.cpp
@@ -1,1 +1,28 @@
 #include <iostream>
+
+using namespace std;
+
+const string lineBreak = "____________________________________________________________________________________";
+
+int question1(){
+  int integer = 5, i = 0, j;
+
+  cout << lineBreak << endl << endl;
+  cout << "Question 1 (While loop)" << endl;
+  cout << endl;
+
+  cout << "Enter a number: "; cin >> integer;
+  cout << endl;
+
+  while(i < integer){
+    j = 0;
+    while(j <= i){
+      cout << "*";
+      j++;
+    }
+    cout << endl;
+    i++;
+  }
+
+  return 0;
+}
