d35beff59127471f34b9d31d84bd1b925823fd41
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-15 17:06:56 +0300

    answer question 2

--- laboratoryExercise007/Arrays.cpp
+++ laboratoryExercise007/Arrays.cpp
@@ -26,3 +26,34 @@ int question1(){
   cout << endl;
   return 0;
 }
+
+int question2(){
+  int input;
+  const string integers[10] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+
+  cout << endl;
+  cout << lineBreak << endl;
+  cout << endl;
+
+  cout << "Question 2" << endl;
+  cout << endl;
+
+  do {
+
+    cout << "Enter a number from (1 - 10): "; cin >> input;
+
+  } while(input > 10 || input <= 0);
+
+  cout << endl;
+
+  for(int i = 0; i < input; i++){
+    i > 0 ? cout << string(input - i, ' ') : cout << string(input, ' ');
+    for(int j = 0; j <= i; j++){
+      cout << " " << integers[i];
+    }
+    cout << endl;
+  }
+  cout << endl;
+
+  return 0;
+}
