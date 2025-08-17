60844429ff6b67aed9de21685fd92d2396904d07
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-18 01:49:01 +0300

    answer question 3

--- laboratoryExercise008/Functions.cpp
+++ laboratoryExercise008/Functions.cpp
@@ -84,3 +84,46 @@ int question2(short num = 0){
 
   return 0;
 }
+
+void calculateFibb(int num, vector<int> arrayNum, int num1, int num2){
+  for(int i = 0; i < num; i++){
+    arrayNum.push_back(num1);
+    int nextNum = num1 + num2;
+    num1 = num2;
+    num2 = nextNum;
+  }
+
+  cout << "Fibonacci of " << num << ":  ";
+  for(int j = 0; j < arrayNum.size(); j++){
+    if(j == arrayNum.size() - 1){
+      cout << arrayNum[j] << endl;
+    } else {
+      cout << arrayNum[j] << ", ";
+    }
+  }
+}
+
+int question3(int num = 0){
+  int num1 = 0, num2 = 1;
+  vector<int> arrayNum;
+
+  if(num > 0){
+    cout << lineBreak << endl << endl;
+    cout << "Question 3 (Only Arguments)" << endl << endl;
+    calculateFibb(num, arrayNum, num1, num2);
+    return 0;
+  }
+
+  cout << lineBreak << endl << endl;
+  cout << "Question 3 (User Input)" << endl << endl;
+
+  do {
+    cout << "Enter a number greater than 0: "; cin >> num;
+  } while(num <= 0);
+  cout << endl;
+
+  calculateFibb(num, arrayNum, num1, num2);
+
+  cout << endl;
+  return 0;
+}
