7163d01b42c6b5b1bac82aaf6eddcef9164c6d99
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-23 17:36:59 +0300

    add comments

--- MachineProblem003/paySlip.cpp
+++ MachineProblem003/paySlip.cpp
@@ -1,56 +1,72 @@
-#include "paySlipClass.h"
-#include <iostream>
-#include <iomanip>
+#include "paySlipClass.h" // import the all the classes inside "paySlipClass.h"
+#include <iostream> // enables cout, cin
+#include <iomanip> // enables setprecision
 
+// declare std to use cout, cin, and other data types without using the prefix 'std::'
 using namespace std;
 
+// reusable line break
 const string lineBreak = "____________________________________________________________________";
 
+// formats the number with decimal
 string formatNumber(double num){
   stringstream fixedNum;
 
+  // up to 2 decimal places
   fixedNum << fixed << setprecision(2) << num;
 
+  // convert to the formatted number string
   string lengthOfNum = fixedNum.str();
+  
+  // find the decimal place
   int decimalPlace = lengthOfNum.find(".");
 
+  // insert commas
   for(int i = lengthOfNum.length() - 3; i > 0; i-=3){
     if(i == decimalPlace){ continue; }
     lengthOfNum.insert(i, ",");
   }
 
+  // return the formatted number
   return "Php " + lengthOfNum;
 }
 
 int main(){
+  // create an instance of the class
   PaySlip paySlip;
 
+  // variables for inputs
   string name;
   double salary;
   int overtimeHours;
 
+  // add a line break
   cout << lineBreak << endl << endl;
 
+  // name input
   cout << "Enter your Name: "; getline(cin >> ws, name);
-
   paySlip.setName(name);
+
   do {
+    // salary input
     cout << "Enter your Salary (not less than 10000): "; cin >> salary;
   } while (salary < 10000);
-  
   paySlip.setBasicSalary(salary);
 
   do {
+    // overtime hours input
     cout << "Enter your Overtime Hours (not less than 1): "; cin >> overtimeHours;
   } while (overtimeHours < 1);
-
   paySlip.setOvertimeHours(overtimeHours);
 
+  // compute overall
   paySlip.determinePayGradeAndTaxRate();
   paySlip.compute();
 
+  // add a line break
   cout << lineBreak << endl << endl;
   
+  // show the output
   cout << "Employee Name		  : " << paySlip.getName() << endl;
   cout << "Basic Salary		  : " << formatNumber(paySlip.getBasicSalary()) << endl;
   cout << "Pay Grade		  : " << paySlip.getPayGrade() << endl;
--- MachineProblem003/paySlipClass.h
+++ MachineProblem003/paySlipClass.h
@@ -1,11 +1,14 @@
-#ifndef _PAYSLIP
+#ifndef _PAYSLIP // directives
 #define _PAYSLIP
 
+// enables string
 #include <string>
 
+// enables cout, cin
 using namespace std;
 
 class PaySlip {
+  // private attributes
   private:
     string mName;
     string mPayGrade;
@@ -17,16 +20,22 @@ class PaySlip {
     double mNetPay;
     double mWithholdingTax;
 
+    // fixed values inside the private scope
     const double SSS = 500.00;
     const double PagIbig = 200.00;
     const double PhilHealth = 100.00;
+  
+  // public methods
   public:
+    // default constructor meaning no arguments we rely on setters
     PaySlip(){}
 
+    // setters (mutators)
     void setName(string name){ mName = name; };
     void setBasicSalary(double salary){ mBasicSalary = salary; };
     void setOvertimeHours(int overtimeHours){ mOvertimeHours = overtimeHours; };
 
+    // getters (accessors)
     string getName(){ return mName; };
     string getPayGrade(){ return mPayGrade; };
     double getBasicSalary(){ return mBasicSalary; };
@@ -36,6 +45,7 @@ class PaySlip {
     double getNetPay(){ return mNetPay; };
     double getWithholdingTax(){ return mWithholdingTax; };
 
+    // find the pay grade and tax rate
     void determinePayGradeAndTaxRate(){
       if(mBasicSalary >= 10000 && mBasicSalary < 15000){
         mPayGrade = "A";
@@ -70,6 +80,7 @@ class PaySlip {
       }
     };
 
+    // computes the pay
     void compute(){
       mOvertimePay = mOvertimeHours * (mBasicSalary * 0.01);
       mGrossPay = mBasicSalary + mOvertimePay;
