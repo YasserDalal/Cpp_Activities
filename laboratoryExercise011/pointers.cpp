c94a8e7baa7a7fc1907c1f19dd7ac1a9d4feb2cd
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-25 00:11:44 +0300

    answer question 1

--- laboratoryExercise011/pointers.cpp
+++ laboratoryExercise011/pointers.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+
+using namespace std;
+
+const string lineBreak = "_________________________________________________________________";
+
+int main() {
+  // -----------------------------   Question 1   ----------------------------------------------
+  int num = 900;
+  int *pNum = &num;
+
+  cout << lineBreak << endl << endl;
+  cout << "Question 1 " << "( num" << " = " << num << " )" << endl << endl;
+
+  cout << "Value: " << *pNum << endl;
+  cout << "Address: " << pNum << endl << endl;
+  
+  return 0;
+}
