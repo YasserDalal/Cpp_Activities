067a0e228e459222e20944859a5a2a4a3272fad6
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-25 00:52:51 +0300

    add comments

--- laboratoryExercise011/pointers.cpp
+++ laboratoryExercise011/pointers.cpp
@@ -1,14 +1,17 @@
-#include <iostream>
+#include <iostream> // enables cout
 
+// declare std to use cout and other data types without using the prefix 'std::'
 using namespace std;
 
+// reusable line break
 const string lineBreak = "_________________________________________________________________";
 
 int main() {
   // -----------------------------   Question 1   ----------------------------------------------
-  int num = 900;
-  int *pNum = &num;
+  int num = 900; // value
+  int *pNum = &num; // pointer (& = address, * = value)
 
+  // show the output
   cout << lineBreak << endl << endl;
   cout << "Question 1 " << "( num" << " = " << num << " )" << endl << endl;
 
@@ -16,9 +19,10 @@ int main() {
   cout << "Address: " << pNum << endl << endl;
   
   // -----------------------------   Question 2   ----------------------------------------------
-  char array[10];
-  char *pArray = array;
+  char array[10]; // size 10 array
+  char *pArray = array; // pointer (array itself has the address)
 
+  // show the output
   cout << lineBreak << endl << endl;
   cout << "Question 2 " << "( array[10] )" << endl << endl;
 
@@ -26,9 +30,10 @@ int main() {
   cout << "Address: " << (string*)pArray << endl << endl;
 
   // -----------------------------   Question 3   ----------------------------------------------
-  float largeArray[20];
-  float *pLargeArray = largeArray;
+  float largeArray[20]; // size 20 array
+  float *pLargeArray = largeArray; // pointer (array itself already has the address)
 
+  // show the output
   cout << lineBreak << endl << endl;
   cout << "Question 3 " << "( array[20] )" << endl << endl;
 
