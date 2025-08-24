d3508446452c2a73920e0be065ad043ad9962045
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-25 00:13:16 +0300

    answer question 2

--- laboratoryExercise011/pointers.cpp
+++ laboratoryExercise011/pointers.cpp
@@ -15,5 +15,15 @@ int main() {
   cout << "Value: " << *pNum << endl;
   cout << "Address: " << pNum << endl << endl;
   
+  // -----------------------------   Question 2   ----------------------------------------------
+  char array[10];
+  char *pArray = array;
+
+  cout << lineBreak << endl << endl;
+  cout << "Question 2 " << "( array[10] )" << endl << endl;
+
+  cout << "Value: " << (int)*pArray << endl;
+  cout << "Address: " << (string*)pArray << endl << endl;
+
   return 0;
 }
