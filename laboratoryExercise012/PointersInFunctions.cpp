3b1f670316a98647ce7858b87ef0159150932423
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-26 18:03:09 +0300

    answer question 1

--- laboratoryExercise012/PointersInFunctions.cpp
+++ laboratoryExercise012/PointersInFunctions.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+
+using namespace std;
+
+// reusable line break
+const string lineBreak = "_______________________________________________________________________";
+
+void Question1(int &num){
+  int *pNum = &num;
+  *pNum = 27946;
+}
+
+int main(){
+  // -----------------------------   Question 1   ----------------------------------------------
+  cout << lineBreak << endl << endl;
+  int num = 654321;
+
+  cout << "Question 1" << " ( num" << " = " << num << " )" << endl << endl;
+
+  cout << "Previous Value of num: " << num << endl << endl;
+
+  Question1(num);
+
+  cout << "Current Value of num: " << num << endl << endl;
+
+  return 0;
+}
