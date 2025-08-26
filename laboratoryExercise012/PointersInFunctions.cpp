d33b49d5422cf749f47419ca9a92d7c3a6c6f040
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-08-26 18:46:04 +0300

    answer question 2

--- laboratoryExercise012/PointersInFunctions.cpp
+++ laboratoryExercise012/PointersInFunctions.cpp
@@ -23,5 +23,22 @@ int main(){
 
   cout << "Current Value of num: " << num << endl << endl;
 
+  // -----------------------------   Question 2   ----------------------------------------------
+  cout << lineBreak << endl << endl;
+  cout << "Question 2" << endl << endl;
+
+  float floatNumber, sharedDetails = 55.5;
+  
+  float *pFloatNumber = &floatNumber;
+
+  *pFloatNumber = sharedDetails;
+  pFloatNumber = &sharedDetails;
+
+  cout << "floatNumber Value: " << floatNumber << endl;
+  cout << "floatNumber Address: " << pFloatNumber << endl << endl;
+
+  cout << "sharedDetails Value: " << sharedDetails << endl;
+  cout << "sharedDetails Address: " << &sharedDetails << endl << endl;
+
   return 0;
 }
